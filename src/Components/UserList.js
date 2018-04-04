fddac9a1d8d995a7bb5638bee6414ef2d0887338
Sabrina Fiester <sabrina.fiester@gmail.com>
2018-04-03 23:23:38 -0500

    Use v4 api

--- src/Components/UserList.js
+++ src/Components/UserList.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import {List, ListItem} from 'material-ui/List';
-import Avatar from 'material-ui/Avatar';
 
 
 export class UserList extends React.Component {
@@ -13,10 +11,8 @@ export class UserList extends React.Component {
     this.props.chooseUser(userKey);
   }
     renderUsers() {
-        return this.props.users.map((user, index) => (
-            <li id={index} key={index} onClick={this.handleClick} className="list-group-item list-group-item-action">{user.login}</li>
-            // <ListItem key={index} primaryText={user.login} onClick={this.handleClick} />
-            )
+        return this.props.users.map((user, index) => (<li id={index} key={index} onClick={this.handleClick}
+                className="list-group-item list-group-item-action" >{user.login}</li>)
     )}
     render() {
         let content = "Loading..";
